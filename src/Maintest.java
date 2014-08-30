1a71fc75fb160b8e0422562e70298b8da64a22c1
yuhailei1992 <yuhailei1992@163.com>
2014-08-30 11:53:38 -0400

    solved 3 problems

--- src/Maintest.java
+++ src/Maintest.java
@@ -4,7 +4,6 @@ public class Maintest {
 	
 	public static void main(String[] args) {
 		System.out.println("Begin test...");
-		Problems.P1();
-		Problems.reverseWords();
+		Problems.reverseInteger();
 	}
 }
--- src/problems/Problems.java
+++ src/problems/Problems.java
@@ -1,27 +1,79 @@
 package problems;
 
+import java.util.Stack;
+
 public class Problems {
-	public static void P1 () {
-		System.out.println("Hello world");
-	}
-	String s = "the sky   is blue";
-	public static String reverseWords() {
+	public static void P1 () {//pass
 		String s = "    the sky is blue";
-        s = s.trim();
+        s = s.trim();//remove leading and trailing spaces
         System.out.println(s);
-        String buf = "";
-        String[] splited = s.split("\\s+");
-        int len = splited.length;
-        System.out.println(len);
-        for (int i = splited.length - 1; i >= 0; --i) {
-        	buf += splited[i];
+        String buf = "";//string buffer
+        String[] strsplit = s.split("\\s+");
+        for (int i = strsplit.length - 1; i >= 0; --i) {
+        	buf += strsplit[i];
         	if (i != 0) {
         		buf += " ";
         	}
-        	System.out.println(splited[i]);
-        	System.out.println(splited[i].length());
+        	System.out.println(strsplit[i]);
+        	System.out.println(strsplit[i].length());
         }
         System.out.println(buf);
-        return s;
     }
+	
+	public static void P2 () {//pass
+		String[] tokens = {"3", "-4", "+"};
+		int len = tokens.length;
+		String[] stk = new String[len];
+		String ops = "+-*/";		
+		int stkptr = 0;
+		for (int i = 0; i < len; ++i) {
+			if (!ops.contains(tokens[i])) {
+				stk[stkptr] = tokens[i];
+				stkptr++;
+			}
+			else {
+				int temp = 0;
+				int index = ops.indexOf(tokens[i]);
+				switch (index) {
+					case 0: temp = Integer.parseInt(stk[stkptr - 2]) + Integer.parseInt(stk[stkptr - 1]);
+						break;
+					case 1: temp = Integer.parseInt(stk[stkptr - 2]) - Integer.parseInt(stk[stkptr - 1]);
+						break;
+					case 2: temp = Integer.parseInt(stk[stkptr - 2]) * Integer.parseInt(stk[stkptr - 1]);
+						break;
+					case 3: temp = Integer.parseInt(stk[stkptr - 2]) / Integer.parseInt(stk[stkptr - 1]);
+						break;
+					default :
+						break;
+				}
+				stk[stkptr - 2] = Integer.toString(temp);
+				stkptr--;
+			}
+		}
+		System.out.println(stk[0]);
+	}
+	
+	public static void P3 () {
+		
+	}
+	
+	public static void reverseInteger () {//pass
+		int x = -321;
+		int isNeg = 0;
+		int x_new = 0;
+		if (x < 0) {
+			x = -x;
+			isNeg = 1;
+		}
+		while (x != 0) {
+			int m = x % 10;
+			x_new = x_new * 10;
+			x_new += m;
+			x = x / 10;
+		}
+		if (isNeg == 1) {
+			x_new = -x_new;
+		}
+		System.out.println(x_new);
+	}
 }
