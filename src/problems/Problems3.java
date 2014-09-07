9e5c557cbde25791c12dde256e0fa2585b077116
yuhailei1992 <yuhailei1992@163.com>
2014-09-07 01:19:46 -0400

    problem 35 solved

--- src/problems/Problems3.java
+++ src/problems/Problems3.java
@@ -183,6 +183,19 @@ public class Problems3 {
     	p.next = null;
     	return newhead;
     }
+    
+    public static void rotate(int[][] matrix) {//accepted after consulting
+        int n = matrix[0].length;
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = i; j < n - 1 - i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = temp;
+            }
+        }
+    }
     public static void test () {
     	ListNode n1 = new ListNode(1);
     	ListNode n2 = new ListNode(2);
