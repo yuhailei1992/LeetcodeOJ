8e4e012ac08d274876045599dc9b70984d51916f
yuhailei1992 <yuhailei1992@163.com>
2014-09-08 17:06:17 -0400

    problem 42 solved after consulting the answer
    The answer on fisherlei.blogspot.com is elegant!

--- src/problems/Problems4.java
+++ src/problems/Problems4.java
@@ -7,12 +7,28 @@ public class Problems4 {
         return sym(root.left, root.right);
     }
     public static boolean sym(TreeNode left, TreeNode right) {
-        if (left == null) return right == null;
+        if (left == null) return right == null;//these two lines are concise!
         if (right == null) return left == null;
         if (left.val != right.val) return false;
         if (sym(left.left, right.right) && sym(left.right, right.left)) {
             return true;
         }
         return false;
+    }
+    
+    public int minDepth(TreeNode root) {//solved after consulting the answer
+        if (root == null) return 0;
+        int ldepth = minDepth(root.left);
+        int rdepth = minDepth(root.right);
+        if (ldepth == 0 && rdepth == 0) {
+            return 1;
+        }
+        if (ldepth == 0) {
+            ldepth = Integer.MAX_VALUE;
+        }
+        if (rdepth == 0) {
+            rdepth = Integer.MAX_VALUE;
+        }
+        return Math.min(ldepth, rdepth) + 1;
     }
 }
