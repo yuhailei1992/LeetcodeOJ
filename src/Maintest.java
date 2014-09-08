fe1442954d2fe68a41489706057f707414455e9b
yuhailei1992 <yuhailei1992@163.com>
2014-09-08 18:13:45 -0400

    reverse_n problem solved!

--- src/Maintest.java
+++ src/Maintest.java
@@ -1,9 +1,10 @@
 import problems.DynamicProgramming;
+import problems.LinkedList;
 import problems.Problems3;
 public class Maintest {
 	
 	public static void main(String[] args) {
 		System.out.println("Begin test...");
-		Problems3.test();
+		LinkedList.testLinkedList();
 	}
 }
