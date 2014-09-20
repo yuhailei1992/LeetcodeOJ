245fc7c7a35c238ff4c74e2fc817fe4f02b2de8c
yuhailei1992 <yuhailei1992@163.com>
2014-09-19 21:40:40 -0400

    problem 55 accepted
    dynamic programming

--- src/problems/Problems5.java
+++ src/problems/Problems5.java
@@ -1,6 +1,6 @@
 package problems;
 import java.util.*;
-
+import java.util.Arrays;
 import problems.Datastructures.*;
 
 
@@ -157,6 +157,22 @@ public class Problems5 {
         showArray(A);
     }
 
+	public static int uniquePaths(int m, int n) {//accepted
+		int[][] map = new int[m][n];
+		for (int i = 0; i < m; ++i) {
+			map[i][0] = 1;
+		}
+		for (int j = 0; j < n; ++j) {
+			map[0][j] = 1;
+		}
+		for (int i = 1; i < m; ++i) {
+			for (int j = 1; j < n; ++j) {
+				map[i][j] = map[i-1][j]+map[i][j-1];
+			}
+		}
+		return map[m-1][n-1];
+    }
+	
 	public static void showArray (int[] n) {
 		for (int i = 0; i < n.length; ++i) {
 			System.out.print(n[i]);
@@ -165,8 +181,7 @@ public class Problems5 {
 	}
 	
 	public static void test () {
-		int n[] = {2, 1, 0};
-		sortColors(n);
+		System.out.println(uniquePaths(1, 2));
 	}
 
 }
