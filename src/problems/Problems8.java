ea93f7199a8374a925176732213579b477d70d89
yuhailei1992 <yuhailei1992@163.com>
2014-10-08 17:14:53 -0400

    problem 88 AC after consulting

--- src/problems/Problems8.java
+++ src/problems/Problems8.java
@@ -393,6 +393,24 @@ public class Problems8 {
         }
         return lli;
     }
+    
+    public static int maxPathSum(TreeNode root) {//AC after consulting
+        if (root == null) return 0;
+        int max[] = new int[]{Integer.MIN_VALUE};
+        maxPath(root, max);
+        return max[0];
+    }
+    
+    private static int maxPath(TreeNode root, int[] max) {
+    	if (root == null) return 0;
+    	int left = maxPath(root.left, max);
+    	int right = maxPath(root.right, max);
+    	int arch = left + right + root.val;//what if left or right is negative?
+    	int ret = Math.max(root.val, Math.max(left, right) + root.val);
+    	max[0] = Math.max(max[0], Math.max(arch, ret));
+    	return ret;
+    }
+
     public static void test () {
         TreeNode n0 = new TreeNode(3);
     	TreeNode n1 = new TreeNode(1);
