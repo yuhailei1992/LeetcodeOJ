8430d0c8d5ca8c836e0b08357d2dd384a447c2f3
yuhailei1992 <yuhailei1992@163.com>
2014-10-30 21:33:06 -0400

    problem 110 AC

--- src/problems/Problems10.java
+++ src/problems/Problems10.java
@@ -509,11 +509,32 @@ public class Problems10 {
     	}
     }
     
-    public static String getPerm (int n, int k) {
-    	int t1 = factorial(n-1);
-    	int div = n / t1;
-    	int rem = n % t1;
-    	return null;
+    public static String getPerm (int n, int k) {//AC
+    	//form the list
+    	ArrayList<Integer> ret = new ArrayList<Integer>();
+
+    	ArrayList<Integer> ls = new ArrayList<Integer>();
+    	for (int i = 0; i < n; ++i) {
+    		ls.add(i+1);
+    	}
+    	
+    	int rem = k-1;
+    	int denom = factorial(ls.size()-1);
+    	int div = rem / denom;
+    	rem = rem % denom;
+    	for (int i = 0; i < n; ++i) {
+    		ret.add(ls.get(div));
+    		ls.remove(div);
+    		denom = factorial(ls.size()-1);
+    		div = rem / denom;
+    		rem = rem % denom;
+    	}
+    	System.out.println(ret);
+    	String res = "";
+    	for (int i = 0; i < ret.size(); ++i) {
+    		res += ret.get(i);
+    	}
+    	return res;
     	
     }
     
@@ -523,48 +544,6 @@ public class Problems10 {
             fact *= i;
         }
         return fact;
-    }
-    
-    public static boolean isInterleave(String s1, String s2, String s3) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-    
-        // Start typing your Java solution below
-        // DO NOT write main() function=
-       if(s3.length()!=s1.length()+s2.length()) return false;    
-        
-       boolean [][] match = new boolean [s1.length()+1][s2.length()+1];
-       match[0][0]=true;
-       
-       int i=1;
-       while(i<=s1.length() && s1.charAt(i-1)==s3.charAt(i-1)){
-           match[i][0]=true;
-           i++;
-       }
-       
-       i=1;
-       while(i<=s2.length() && s2.charAt(i-1)==s3.charAt(i-1)){
-           match[0][i]=true;
-           i++;
-       }
-       
-       for(i=1;i<=s1.length();i++){
-           for(int j=1;j<=s2.length();j++){
-               char c = s3.charAt(i+j-1);
-               if(c==s1.charAt(i-1))
-                match[i][j]=match[i-1][j] || match[i][j];
-               if(c==s2.charAt(j-1))
-                match[i][j]=match[i][j-1] || match[i][j];
-           }
-       }
-       for (i = 0; i <= s1.length(); ++i) {
-    	   for (int j = 0; j <= s2.length(); ++j) {
-    		   if (match[i][j]) System.out.print("*");
-    		   else System.out.print(".");
-    	   }
-    	   System.out.println();
-       }
-      return match[s1.length()][s2.length()];
     }
     
     public static boolean isInterleave2(String s1, String s2, String s3) {//AC
@@ -606,10 +585,6 @@ public class Problems10 {
     }
     
     public static void test () {
-    	int num[] = {1, 2, 3};
-    	String a = "a";
-    	String b = "";
-    	String c = "a";
-    	isInterleave2(a, b, c);
+    	getPerm(4, 17);
     }
 }
