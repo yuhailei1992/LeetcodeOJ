9bc25cebb5bab2e716fa9c18577c9120ba535dbb
yuhailei1992 <yuhailei1992@163.com>
2014-10-31 20:18:41 -0400

    deleted .orig files

---
+++ src/problems/Problems11.java
@@ -0,0 +1,12 @@
+package problems;
+import java.util.*;
+
+public class Problems11 {
+    public List<String> restoreIpAddresses(String s) {
+    	int len = s.length();
+    	if (len < 4 || len > 12) return null;
+    	
+    	
+        return null;
+    }
+}
