7e55243b32626d8640275b56b39246bd475c2072
yuhailei1992 <yuhailei1992@163.com>
2014-11-16 13:20:24 -0500

    problem 149 AC

--- src/Maintest.java
+++ src/Maintest.java
@@ -1,14 +1,14 @@
 import problems.Problems13;
 import problems.Problems14;
+import problems.Problems15;
 import problems.LRUCache;
 
 public class Maintest {
 	
 	public static void main(String[] args) {
-		//Problems13 t = new Problems13();
-		//t.test();
-		Problems14 t2 = new Problems14();
-		t2.test();
+		//Problems13 t = new Problems13(); t.test();
+		//Problems14 t2 = new Problems14(); t2.test();
+		Problems15 t3 = new Problems15(); t3.test();
 		//LRUCache.test();
 	}
 }
--- src/problems/Problems15.java
+++ src/problems/Problems15.java
@@ -29,7 +29,6 @@ public class Problems15 {
     			break;
     		}
     	}
-    	
     	// now we have the index of root node in the inorder traversal
     	int len = index - i_start;
     	root.left = build_sub(preorder, p_start+1, p_start+1+len-1, inorder, i_start, index-1);
@@ -37,10 +36,48 @@ public class Problems15 {
     	return root;
     }
     
+    /*
+     * lesson learned: do not use absolute indexes
+     */
+    public TreeNode buildTree2(int[] inorder, int[] postorder) {//AC
+    	int i_start = 0;
+    	int i_end = inorder.length-1;
+    	int p_start = 0;
+    	int p_end = postorder.length-1;
+    	
+    	return build_sub2(inorder, i_start, i_end, postorder, p_start, p_end);
+    }
+    
+    private TreeNode build_sub2 (int[] inorder, int i_start, int i_end, int[] postorder, int p_start, int p_end)
+    {
+    	if (i_start > i_end || p_start > p_end)
+    		return null;
+    	
+    	TreeNode root = new TreeNode(postorder[p_end]);
+    	
+    	int index = 0;
+    	for (int i = i_start; i <= i_end; ++i)
+    	{
+    		if (inorder[i] == postorder[p_end])
+    		{
+    			index = i;
+    			break;
+    		}
+    	}
+    	
+    	// now we have the index of root node in the inorder traversal
+    	int len = index - i_start;
+    	root.left = build_sub2(inorder, i_start, index-1, postorder, p_start, p_start+len-1);
+    	root.right = build_sub2(inorder, index+1, i_end, postorder, p_start+len, p_end-1);
+    	return root;
+    }
+    
     //152
     
     public void test () 
     {
-    	
+    	int inorder[] = {1, 2, 3, 4, 5};
+    	int postorder[] = {2, 3, 1, 5, 4};
+    	buildTree2(inorder, postorder);
     }
 }
